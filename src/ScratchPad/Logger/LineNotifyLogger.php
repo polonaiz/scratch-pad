95567997fe3d3d7557ccaa30429c3e87ceb2d148
chirichidi <chirichidi@com2us.com>
2020-06-22 18:37:41 +0900

    라인 노티에 재전송 로직 적용

--- src/ScratchPad/Logger/LineNotifyLogger.php
+++ src/ScratchPad/Logger/LineNotifyLogger.php
@@ -2,6 +2,10 @@
 
 namespace ScratchPad\Logger;
 
+use Exception;
+use ScratchPad\Retry;
+use Throwable;
+
 class LineNotifyLogger implements LoggerInterface
 {
     use LoggerImpl;
@@ -15,50 +19,57 @@ class LineNotifyLogger implements LoggerInterface
 
     /**
      * @param array $message
-	 * @param array $option
-     * @throws \Exception
+     * @param array $option
+     * @throws Throwable
      */
     public function log(array $message, array $option = [])
     {
-        $url = "https://notify-api.line.me/api/notify";
-        $token = $this->config['token'] ?? null;
-        $httpHeader = [
-            "Authorization: Bearer {$token}"
-        ];
-        $postFields = "message=" . substr(json_encode($message, JSON_PRETTY_PRINT), 0, 1024);
-        $connectTimeout = $this->config['connectTimeout'] ?? 5;
-        $executeTimeout = $this->config['executionTimeout'] ?? 5;
+        Retry::execute([
+            'maxExecutionCount' => 5,
+            'onExecute' => function() use (&$message, &$executionCount) {
+                $url = "https://notify-api.line.me/api/notify";
+                $token = $this->config['token'] ?? null;
+                $httpHeader = [
+                    "Authorization: Bearer {$token}"
+                ];
+                $postFields = "message=" . substr(json_encode($message, JSON_PRETTY_PRINT), 0, 1024);
+                $connectTimeout = $this->config['connectTimeout'] ?? 5;
+                $executeTimeout = $this->config['executionTimeout'] ?? 10;
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $httpHeader);
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $postFields);
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $connectTimeout);
-        curl_setopt($curl, CURLOPT_TIMEOUT, $executeTimeout);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $httpHeader);
+                curl_setopt($curl, CURLOPT_POST, 1);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $postFields);
+                curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $connectTimeout);
+                curl_setopt($curl, CURLOPT_TIMEOUT, $executeTimeout);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-        $output = curl_exec($curl);
-        $info = curl_getinfo($curl);
-        $statusCode = $info['http_code'];
+                $output = curl_exec($curl);
+                $info = curl_getinfo($curl);
+                $statusCode = $info['http_code'];
 
-        if ($statusCode != 200)
-        {
-            throw new \Exception("FAILURE: " . json_encode([
-                    'message' => 'curl request failed',
-                    'info' => $info,
-                    'url' => $url,
-                    'httpHeader' => $httpHeader,
-                    'postFields' => $postFields,
-                    'status' => $statusCode,
-                    'output' => $output,
-                ]));
-        }
-
-        $data = json_decode($output, true);
-        /** @noinspection PhpUnusedLocalVariableInspection */
-        $status = $data['status'];
-        /** @noinspection PhpUnusedLocalVariableInspection */
-        $message = $data['message'];
+                switch ($statusCode) // https://developers.line.biz/en/reference/messaging-api/#status-codes
+                {
+                    case 0:
+                    case 500:
+                        // required retry
+                        throw new Exception("FAILURE: " . json_encode([
+                                'message' => 'curl request failed',
+                                'info' => $info,
+                                'url' => $url,
+                                'httpHeader' => $httpHeader,
+                                'postFields' => $postFields,
+                                'status' => $statusCode,
+                                'output' => $output,
+                            ]));
+                    default:
+                        /** @noinspection PhpUnusedLocalVariableInspection */
+                        $data = json_decode($output, true);
+                        $status = $data['status'];
+                        $message = $data['message'];
+                }
+            },
+        ]);
     }
 }
